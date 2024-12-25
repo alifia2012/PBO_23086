32c8f9c5c1a33e61a226e9578f19cde52ab137c5
alifia2012 <alifyarisman@gmail.com>
2024-12-25 19:47:18 +0700

    Create modul_kalkulator.py

---
+++ modul_kalkulator.py
@@ -0,0 +1,26 @@
+# buat modul kalkulator untuk program kalkulator sederhana
+# ini adalah file modulnya
+
+def tambah(a,b):
+    return (a+b):
+
+def kali(a,b):
+    return (a * b)
+
+
+class kalkulator:
+    def __init__(self,a, b):
+        self.a=a 
+        self.b=b
+
+    def tambah(self):
+        return(self.a + self.b)
+
+    def kali(self):
+        return (self.a * self.b)
+
+    def bagi(self):
+        return (self.a / self.b)
+
+    def kurang(self):
+        return (self.a _)
